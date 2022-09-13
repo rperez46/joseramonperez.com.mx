8c7e7211b0861f154ab445a7e5c8647248dbb5b4
Ramón Pérez <joseramonperez46@gmail.com>
2022-09-13 01:13:16 -0500

    Titulo y favicon

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
 import Header from 'components/Header';
+import Head from 'next/head'
 
 export default () => {
 	return (
 		<>
+			<Head>
+				<title>José Ramón Pérez Herrera</title>
+			</Head>
 			<Header />
 		</>
 	);

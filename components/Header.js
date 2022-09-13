459a13cca846e39af24ab59a80d3165d2820b3e6
Ramón Pérez <joseramonperez46@gmail.com>
2022-09-12 23:36:59 -0500

    Commit inicial

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import Container from 'react-bootstrap/Container';
+
+export default () => {
+	return (
+		<Container>
+			<h1>José Ramón Pérez Herrera</h1>
+			<h2>Desarrollador .NET / ReactJS</h2>
+		</Container>
+	);
+}
